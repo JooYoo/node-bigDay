59f752fe7583b67ca0272185b2e78acab262109b
JooYoo <jooyoo@outlook.com>
2021-02-23 12:12:54 +0100

    07_mongoose_model

---
+++ models/BigDay.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const BigDaySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add title'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add description'],
+  },
+  begin: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add begin time'],
+  },
+  end: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add end time'],
+  },
+  totalDays: {
+    type: Number,
+    required: [true, 'Please add totalDays'],
+  },
+  isHighlight: {
+    type: Boolean,
+    default: false,
+  },
+  themeColor: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add theme color'],
+  },
+});
+
+module.exports = mongoose.model('BigDay', BigDaySchema);
