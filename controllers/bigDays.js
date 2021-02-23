247e063678f3003085e575e16ee4ba06b0ebb60f
JooYoo <jooyoo@outlook.com>
2021-02-23 12:25:01 +0100

    08_controller_get_data_from_db

--- controllers/bigDays.js
+++ controllers/bigDays.js
@@ -1,8 +1,23 @@
+const BigDay = require('../models/BigDay');
+
 // @desc   get all bigDays
 // @route  GET /api/v1/bigDays
 // @access Public
-const getBigDays = (req, res, next) => {
-  res.send('GET bigDays');
+const getBigDays = async (req, res, next) => {
+  try {
+    const bigDays = await BigDay.find();
+
+    return res.status(200).json({
+      success: true,
+      count: bigDays.length,
+      data: bigDays,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 // @desc   add new bigDay
